82027e9dab49f3bfff1dbcbe223a10d98c22634f
unknown <7dospredios@gmail.com>
2023-04-06 15:03:36 -0300

    realizei a separacao dos impares e pares

---
+++ quinto-exercicio.py
@@ -0,0 +1,15 @@
+exerc5 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20']
+print(exerc5)
+for impares in exerc5:
+    if impares not in "2, 4, 6, 8, 10, 12, 14, 16, 18, 20":
+        print("IMPARES",impares)
+
+
+print("###############################")
+
+for pares in exerc5:
+    if pares not in "1, 3, 5, 7, 9, 11, 13, 15, 17, 19":
+        print("PARES", pares)
+
+
+
