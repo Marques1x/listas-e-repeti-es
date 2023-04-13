272719d87a6418303336087c83b5e4725089a1ef
unknown <7dospredios@gmail.com>
2023-04-13 14:40:42 -0300

    Programa que peça as quatro notas de 10 alunos, com média maior ou igual a 7.0& C:/Users/21/AppData/Local/Programs/Python/Python311/python.exe c:/dev/listas-e-repeti-es/sexto-exercicio.py

--- quinto-exercicio.py
+++ quinto-exercicio.py
@@ -2,7 +2,7 @@ exerc5 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16'
 print(exerc5)
 for impares in exerc5:
     if impares not in "2, 4, 6, 8, 10, 12, 14, 16, 18, 20":
-        print("IMPARES",impares)
+        print("IMPARES", impares)
 
 
 print("###############################")
